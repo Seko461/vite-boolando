b23237f68f7ce19f9fda8840a3c84710b3171f19
Seko461 <sekopierre@gmail.com>
2024-03-22 17:12:03 +0100

    final MILESTONE 2 commit

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'ProductCard',
+    props: ['product']
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="item">
+            <img class="a" :src="product.imageA" width="100%" alt="">
+            <img class="b" :src="product.imageB" width="100%" alt="">
+            <div class="heart">&hearts;</div>
+            <div class="discount">{{ product.discount }}</div>
+            <div class="eco">{{ product.eco }}</div>
+        </div>
+        <div class="description">
+            <div class="label">{{ product.label }}</div>
+            <div class="title">{{ product.title }}</div>
+            <div class="price">
+                <span class="final_price">{{ product.finalPrice }}&euro;</span>
+                <span class="initial_price">{{ product.initialPrice }} &euro;</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
