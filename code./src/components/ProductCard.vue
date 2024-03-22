3f24644ef7c170f0d7b9e80610e49bb1649b4c84
Seko461 <sekopierre@gmail.com>
2024-03-22 12:50:43 +0100

    add cardInfo in an array and create Card structure

---
+++ code./src/components/ProductCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'ProductCard'
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="item">
+            <img class="a" src="/images/1.webp" width="100%" alt="">
+            <img class="b" src="/images/1b.webp" width="100%" alt="">
+            <div class="heart">&hearts;</div>
+            <div class="discount">-50%</div>
+            <div class="eco">Sostenibilità</div>
+        </div>
+        <div class="description">
+            <div class="label">Levi's</div>
+            <div class="title">RELAXED FIT TEE UNISEX</div>
+            <div class="price">
+                <span class="final_price">14,99 &euro;</span>
+                <span class="initial_price">29,99 &euro;</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
